f4d1d0886b06fe0003f23a2e5ca938f4e5f6e3f8
Jack Harter <jack.harter@me.com>
2025-03-25 22:30:12 -0500

    Update Terraform configuration to use aws_subnets data source and upgrade AWS provider version

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 # create an ec2 instance; the instance will be created in the default VPC
-resource "ec2" "typical_instance" {
+resource "aws_instance" "typical_instance" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
-  subnet_id     = data.aws_subnet_ids.default.ids[0]
+  subnet_id     = data.aws_subnets.default.ids[0]
   tags = {
     Name = "TypicalInstance"
   }
